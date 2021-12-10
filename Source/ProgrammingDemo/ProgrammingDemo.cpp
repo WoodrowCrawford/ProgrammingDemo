3ef65a060b574030547c33e0b48ad202677cd59e
WoodrowCrawford <woodrowcrawford03@gmail.com>
2021-12-10 13:55:21 -0600

    Test again

--- Source/ProgrammingDemo/ProgrammingDemo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProgrammingDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProgrammingDemo, "ProgrammingDemo" );
